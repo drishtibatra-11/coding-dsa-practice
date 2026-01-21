a1839d82390c08ff7d8e8a70043fc857a454ba7c
drishtibatra-11 <batradrishti22@gmail.com>
2026-01-21 14:33:04 +0000

    day5.cpp

---
+++ day5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ,i;
+    cout<< "enter the number" << endl;
+    cin >> n;
+    for ( i=0 ; i<=n ; i++ )
+    {
+    cout << "****" << endl;
+    }
+    return 0;
+
+}

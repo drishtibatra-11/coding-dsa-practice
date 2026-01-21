5e77438a7568549497370d00f36659e586037aa0
drishtibatra-11 <batradrishti22@gmail.com>
2026-01-21 15:02:57 +0000

    day5.1.cpp

---
+++ day5.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i;
+    cout << "enter the value of n" << endl;
+    cin >> n;
+    for (i=n; i>=1; i--)
+    {
+        cout<< i << endl;
+    }
+    return 0;
+}

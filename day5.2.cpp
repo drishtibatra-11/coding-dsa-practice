277a5971b03de9e5a091f90673b5b4bd10c0c27e
drishtibatra-11 <batradrishti22@gmail.com>
2026-01-22 18:20:41 +0000

    day5.2.cpp

---
+++ day5.2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int var;
+    cout << "enter a variable" << endl;
+    cin >> var ;
+    if (var%2==0)
+    {
+    cout << "even number" << endl;
+    }
+    else
+{
+    cout << "odd number" << endl;
+}
+return 0;
+}

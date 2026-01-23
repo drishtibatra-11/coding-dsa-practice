5f5bffd390861f27f6c6d863af2be44d7f901249
drishtibatra-11 <batradrishti22@gmail.com>
2026-01-23 18:29:49 +0000

    day5.3.cpp

---
+++ day5.3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name;
+    int age;
+cout << "enter your name" << endl;
+cin >> name;
+cout << "enter your age" << endl;
+cin>>age ;
+return 0;
+}
